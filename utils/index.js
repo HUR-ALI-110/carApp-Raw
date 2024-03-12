34c662ee4ccbac260f88ac6d00763b50294423c1
HUR-ALI-110 <alinasri198k@gmail.com>
2024-03-12 08:07:59 +0500

    first commit

--- utils/index.js
+++ utils/index.js
@@ -1,38 +1,36 @@
 export const generateCarImageUrl = (car, angle) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
+    const url = new URL("https://cdn.imagin.studio/getimage");
 
-  const { make, model, year } = car;
-  url.searchParams.append("customer", "hrjavascript-mastery" || "");
-  url.searchParams.append("make", make);
-  url.searchParams.append("modelFamily", model.split(" ")[0]);
-  url.searchParams.append("zoomType", "fullscreen");
-  url.searchParams.append("modelYear", `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append("angle", `${angle}`);
+    const { make, model, year } = car;
+    url.searchParams.append("customer", "hrjavascript-mastery" || "");
+    url.searchParams.append("make", make);
+    url.searchParams.append("modelFamily", model.split(" ")[0]);
+    url.searchParams.append("zoomType", "fullscreen");
+    url.searchParams.append("modelYear", `${year}`);
+    // url.searchParams.append('zoomLevel', zoomLevel);
+    url.searchParams.append("angle", `${angle}`);
 
-  return `${url}`;
+    return `${url}`;
 };
 
 export async function fetchCars(searchParams) {
-  const { manufacturer, year, fuel, limit, model } = searchParams;
+    const { manufacturer, year, fuel, limit, model } = searchParams;
 
-  // Set the required headers for the API request
-  const headers = {
-    "X-RapidAPI-Key":
-      "a68aa55f2cmsh9bc5df55b9c2fc3p1e7553jsn387e47313763" || "",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  };
+    // Set the required headers for the API request
+    const headers = {
+        "X-RapidAPI-Key": "a68aa55f2cmsh9bc5df55b9c2fc3p1e7553jsn387e47313763" || "",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    };
 
-  // Set the required headers for the API request
-  const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-    {
-      headers: headers,
-    }
-  );
+    // Set the required headers for the API request
+    const response = await fetch(
+        `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+            headers: headers,
+        }
+    );
 
-  // Parse the response as JSON
-  const result = await response.json();
+    // Parse the response as JSON
+    const result = await response.json();
 
-  return result;
+    return result;
 }
